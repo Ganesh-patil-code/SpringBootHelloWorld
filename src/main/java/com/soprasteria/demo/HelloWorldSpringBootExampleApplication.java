cb3e79e1a6b3aca2c9e96b0f771eb48a9e400413
ganesh <ganesh@DESKTOP-VEMMAQK>
2020-10-17 16:58:36 +0530

    WH-8520 - First Time Commit

---
+++ src/main/java/com/soprasteria/demo/HelloWorldSpringBootExampleApplication.java
@@ -0,0 +1,13 @@
+package com.soprasteria.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldSpringBootExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldSpringBootExampleApplication.class, args);
+	}
+
+}
